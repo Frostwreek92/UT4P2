47c78e8da727f816b5aaf4c7a17cbfaf5b08fc3d
Frostwreek92 <frostwreek@gmail.com>
2025-12-20 10:12:19 +0100

    Animaciones Ejercicio3 Hecho, aunque cara no se alinea centro

--- app/src/main/java/com/example/ut4p2/ui/screens/LlamadasScreen.kt
+++ app/src/main/java/com/example/ut4p2/ui/screens/LlamadasScreen.kt
@@ -1,14 +1,39 @@
 package com.example.ut4p2.ui.screens
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.animation.graphics.res.animatedVectorResource
+import androidx.compose.animation.graphics.res.rememberAnimatedVectorPainter
+import androidx.compose.animation.graphics.vector.AnimatedImageVector
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.ut4p2.R
 
 @Composable
 fun LlamadasScreen() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = androidx.compose.ui.Alignment.Center) {
-        Text(text = "Pantalla de Llamadas")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        val image = AnimatedImageVector.animatedVectorResource(R.drawable.avd_sad_to_happy)
+        var atEnd by remember { mutableStateOf(false) }
+        Image(
+            painter = rememberAnimatedVectorPainter(image, atEnd),
+            contentDescription = "VectorDrawable",
+            modifier = Modifier.clickable {
+                atEnd = !atEnd
+            }.size(600.dp)
+        )
     }
 }
--- app/src/main/java/com/example/ut4p2/ui/screens/NovedadesScreen.kt
+++ app/src/main/java/com/example/ut4p2/ui/screens/NovedadesScreen.kt
@@ -5,20 +5,29 @@ import androidx.compose.animation.graphics.res.rememberAnimatedVectorPainter
 import androidx.compose.animation.graphics.vector.AnimatedImageVector
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.ut4p2.R
 
 @Composable
 fun NovedadesScreen() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = androidx.compose.ui.Alignment.Center) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
         val image = AnimatedImageVector.animatedVectorResource(R.drawable.avd_pause_to_play)
         var atEnd by remember { mutableStateOf(false) }
         Image(
@@ -26,8 +35,7 @@ fun NovedadesScreen() {
             contentDescription = "VectorDrawable",
             modifier = Modifier.clickable {
                 atEnd = !atEnd
-            }
-                .fillMaxSize(),
+            }.fillMaxSize()
         )
     }
 }
